ed121dcd998e886408ae3af47f95831044b15e01
Luca Fischer <lucafischer@fastmail.com>
2023-03-20 20:30:43 +0100

    Check it out

--- pipefork_tutorial/pipefork_exec_child_multiple.c
+++ pipefork_tutorial/pipefork_exec_child_multiple.c
@@ -17,11 +17,11 @@ int main(void)
         {
             printf("My pid is %d and I'll exec cmd %d\n", pid, i);
             /* I put a break to simulate the fact that after the exec no line is executed */
-            break ;
+            return 1;
         }
     }
     wait(NULL);
     /* I print only parents because the children got caught in the exec */
-    if (pid > 0)
-        printf("Here I am! My pid is %d\n", pid);
+    
+    printf("Here I am! My pid is %d\n", pid);
 }
