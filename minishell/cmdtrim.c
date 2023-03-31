fd00668d87284a5f70374155a832e023e17b79bd
Timoti Fregni <tfregni@c4c4c3.42berlin.de>
2023-03-31 22:03:57 +0200

    bullshit possible new approach to count_commands

--- minishell/cmdtrim.c
+++ minishell/cmdtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmdtrim.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
+/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 17:51:42 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/03/31 18:08:47 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/03/31 21:59:59 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,57 +89,138 @@ int count_cmds(char *cmds)
 	return (count);
 }
 
-t_bool is_new_cmd(int *state, char *cmds, int i)
+// t_bool is_new_cmd(int *state, char *cmds, int i)
+// {
+// 	if (*state == IN_NORMAL)
+// 	{
+// 	}
+// 	else if (*state == IN_D_QUOTE)
+// 	{
+
+// 	}
+// 	else if (*state == IN_S_QUOTE)
+// 	{
+// 	}
+// }
+
+// void populate_cmds(char **arr, int n_cmds, char *cmds)
+// {
+// 	int i;
+// 	int state;
+
+// 	i = 0;
+// 	state = IN_NORMAL;
+// 	while (str[i] && n_cmds > 0)
+// 	{
+// 		if (is_new_cmd(&state, cmds, i))
+
+// 			i++;
+// 	}
+// }
+
+// char **cmd_trim(char *cmds)
+// {
+// 	char **arr;
+// 	int n_cmds;
+// 	int i;
+
+// 	if (!cmds)
+// 		return (NULL);
+// 	n_cmds = count_cmds(cmds);
+// 	printf("n of words: %d\n", n_cmds);
+// 	arr = malloc(sizeof(char *) * (n_cmds + 1));
+// 	if (!arr)
+// 		return (NULL);
+// 	arr[n_cmds] = NULL;
+// 	populate_cmds(arr, n_cmds, cmds);
+// 	return (arr);
+// }
+
+/*
+(*i == 0 && !ft_is_space(cmds[*i])) ||
+(ft_is_space(cmds[*i - 1]) && !ft_is_space(cmds[*i])) ||
+(cmds[*i - 1] == '\"' && !ft_is_space(cmds[*i])) ||
+(cmds[*i - 1] == '\'' && !ft_is_space(cmds[*i])) */
+t_bool	ft_is_cmd(char **str, int *state)
 {
-	if (*state == IN_NORMAL)
+	printf("%c: state %d\n", **str, *state);
+	if (*state == IN_D_QUOTE)
 	{
-	}
-	else if (*state == IN_D_QUOTE)
-	{
-		
+		if (**str == '\"')
+			*state = IN_NORMAL;
+		return (true);
 	}
 	else if (*state == IN_S_QUOTE)
 	{
+		if (**str == '\'')
+			*state = IN_NORMAL;
+		return (true);
 	}
-}
-
-void populate_cmds(char **arr, int n_cmds, char *cmds)
-{
-	int i;
-	int state;
-
-	i = 0;
-	state = IN_NORMAL;
-	while (str[i] && n_cmds > 0)
+	else
 	{
-		if (is_new_cmd(&state, cmds, i))
-
-			i++;
+		if (ft_is_space(**str))
+			return (false);
+		if (**str == '\'')
+			*state = IN_S_QUOTE;
+		else if (**str == '\"')
+			*state = IN_D_QUOTE;
+		return (true);
 	}
 }
 
-char **cmd_trim(char *cmds)
+int	ft_count_cmds(char *str)
 {
-	char **arr;
-	int n_cmds;
-	int i;
+	int	count;
+	int	state;
+	int	cmd_state;
 
-	if (!cmds)
-		return (NULL);
-	n_cmds = count_cmds(cmds);
-	printf("n of words: %d\n", n_cmds);
-	arr = malloc(sizeof(char *) * (n_cmds + 1));
-	if (!arr)
-		return (NULL);
-	arr[n_cmds] = NULL;
-	populate_cmds(arr, n_cmds, cmds);
-	return (arr);
+	if (!str)
+		return (0);
+	count = 0;
+	state = IN_NORMAL;
+	while (*str)
+	{
+		cmd_state = state;
+		printf("updating cmd_state\n");
+		if (!ft_is_cmd(&str, &state))
+			str++;
+		else
+		{
+			if (state != cmd_state)
+			{
+				str++;
+				continue ;
+			}
+			count++;
+			printf("new cmd starting at %c\n", *str);
+			while (*str && ft_is_cmd(&str, &state) && state == cmd_state)
+			{
+				// printf("inner loop\n");
+				str++;
+			}
+		}
+	}
+	return (count);
+}
+
+void	ft_cmd_trim(char *str)
+{
+	// char	**arr;
+	int		n_cmds;
+
+	n_cmds = ft_count_cmds(str);
+	printf("n_cmds: %d\n", n_cmds);
 }
 
 int main(void)
 {
 	char cmd[] = "\"hello      there\"\"how\"are\'you \'doing? $USER |wc -l >outfile";
-	char cmd[] = "\"hiiiii\"	hey   hi	\"hello\" abc  goodbye      ";
-	cmd_trim(cmd);
+	// char cmd[] = "\"hiiiii\"	hey   hi	\"hello\" abc  goodbye      ";
+	ft_cmd_trim(cmd);
 	// printf("%s\n", cmd);
 }
+
+/*
+"hello      there""how"are'you 'doing? $USER |wc -l >outfile
+1                 2    3  4     5      6     7   8  9
+*/
