da562d218113af023188991c90c580b8cc2e85f3
Luca Fischer <lucafischer@fastmail.com>
2023-03-31 21:28:50 +0200

    add signal

--- minishell/signal.c
+++ minishell/signal.c
@@ -6,7 +6,7 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 13:34:05 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/03/31 20:27:06 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/03/31 20:54:48 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	handler(int signal)
 {
 	if (signal == SIGINT)
-		ft_putstr_fd("Handle SIGINT\n", 1);	
+	{
+		//ft_putstr_fd("Handle SIGINT\n", 1);	
+		rl_replace_line("Hello", 0);			
+	}
 	else if (signal == SIGQUIT)
 		ft_putstr_fd("Handle SIQUIT\n", 1);
 }
