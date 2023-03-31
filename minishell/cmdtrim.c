97fe128abadb83d45513d46cd2e2a0bce513110a
Luca Fischer <lucafischer@fastmail.com>
2023-03-31 13:25:48 +0200

    add cmdtrim

--- minishell/cmdtrim.c
+++ minishell/cmdtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmdtrim.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 17:51:42 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/03/31 09:27:48 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/03/31 13:25:08 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 	// IN_NORMAL,
 	// IN_S_QUOTE,
 	// IN_D_QUOTE,
+
+	//"hey   hi	\"hello\"   goodbye"
 
 void	count_in_normal(char *cmds, int *i, int *count, int *state)
 {
@@ -26,7 +28,7 @@ void	count_in_normal(char *cmds, int *i, int *count, int *state)
 			*state = IN_S_QUOTE;
 		if (*state != IN_NORMAL)
 			break ;
-		if (!ft_is_space(cmds[*i]))
+		if ((*i == 0 && !ft_is_space(cmds[*i])) || (ft_is_space(cmds[*i - 1]) && !ft_is_space(cmds[*i])))
 			*count += 1;
 		*i += 1;
 	}
@@ -45,28 +47,29 @@ int	count_cmds(char *cmds)
 	{
 		if (state == IN_NORMAL)
 			count_in_normal(cmds, &i, &count, &state);
-		else if (state == IN_D_QUOTE);
+		// else if (state == IN_D_QUOTE);
 			//count_in_d_quote()
-		else if (state == IN_S_QUOTE);
+		// else if (state == IN_S_QUOTE);
 			//count_in_s_quote()
 	}
 	return (count);
 }
 
-char	**cmd_trim(char *cmds)
+void	cmd_trim(char *cmds)
 {
 	int		n_cmds;
-	char	**arr;
+	// char	**arr;
 
 	if (!cmds)
-		return (NULL);
+		return ;
 	n_cmds = count_cmds(cmds);
+	printf("n of words: %d\n",n_cmds);
 }
 
 int	main(void)
 {
 	// char cmd[] = "\"hello      there\" how are \'you \'doing? $USER |wc -l >outfile";
-	char cmd[] = "hey   hi	\"hello\"   goodbye";
-
-	printf("%s\n", cmd);
+	char cmd[] = "	hey   hi	\"hello\"   goodbye      ";
+	cmd_trim(cmd);
+	//printf("%s\n", cmd);
 }
