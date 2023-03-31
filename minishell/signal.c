81fb363b8c4b2e2e500aae62a849f354f66eecc7
Luca Fischer <lucafischer@fastmail.com>
2023-03-31 20:24:05 +0200

    add signal

--- minishell/signal.c
+++ minishell/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 13:34:05 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/03/31 09:31:09 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/03/31 19:25:28 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	handler(int signal)
 {
-	if (signal == SIGINT);
-	else if (signal == SIGQUIT);
-	else if ();
+	if (signal == SIGINT){
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		// ft_putstr_fd("Handle SIGINT\n", 1);	
+	}
+	else if (signal == SIGQUIT)
+		ft_putstr_fd("Handle SIQUIT\n", 1);
 }
-
 void	init_act(struct sigaction *act, void (*handler)(int))
 {
 	(*act).sa_handler = handler;
@@ -30,4 +33,6 @@ void	init_signal()
 {
 	struct sigaction	act;
 	init_act(&act, &handler);
+	sigaction(SIGINT, &act, NULL);
+	sigaction(SIGQUIT, &act, NULL);
 }
