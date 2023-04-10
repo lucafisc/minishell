958dcc77277e2560f251c38a466c2ba04c931c12
tim <timotif@gmail.com>
2023-04-10 12:44:36 +0200

    refactored ft_export

--- minishell/builtins/export.c
+++ minishell/builtins/export.c
@@ -6,41 +6,41 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 15:19:22 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/08 17:53:34 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/04/10 12:43:28 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/* test function to modify the environment */
-void ft_changeenv(char **env)
-{
-	while (*env)
-	{
-		if (!ft_strncmp(*env, "arg", 3))
-		{
-			char new_arg[] = "arg=changed";
-			*env = new_arg;
-		}
-		env++;
-	}
-}
+// /* test function to modify the environment */
+// void ft_changeenv(char **env)
+// {
+// 	while (*env)
+// 	{
+// 		if (!ft_strncmp(*env, "arg", 3))
+// 		{
+// 			char new_arg[] = "arg=changed";
+// 			*env = new_arg;
+// 		}
+// 		env++;
+// 	}
+// }
 
-/* test function to make the original env matrix
-pointing to a newly allocated one */
-void	ft_mispoint(char ***env)
-{
-	char	**new;
+// /* test function to make the original env matrix
+// pointing to a newly allocated one */
+// void	ft_mispoint(char ***env)
+// {
+// 	char	**new;
 
-	new = malloc(sizeof(*new) * 3);
-	new[0] = ft_strdup("hello");
-	new[1] = ft_strdup("world");
-	new[2] = NULL;
-	*env = new;
-}
+// 	new = malloc(sizeof(*new) * 3);
+// 	new[0] = ft_strdup("hello");
+// 	new[1] = ft_strdup("world");
+// 	new[2] = NULL;
+// 	*env = new;
+// }
 
 /* returns an allocated 0-terminated copy of the env */
-char **env_dup(char **env)
+char	**env_dup(char **env)
 {
 	int		len;
 	char	**new;
@@ -132,17 +132,21 @@ static void	ft_export_append(char ***env, char *var)
 	//ft_print_strarr(new_env);
 }
 
-void	ft_export(char ***env, char *var)
+void	ft_export(t_shell *s, t_command *c)
 {
 	int		var_index;
+	char	*var;
 
+	if (!c || !s || !c->cmd || !c->cmd[0])
+		return ;
+	var = c->cmd[1];
 	if (!is_export_valid(var))
 		return ;
-	var_index = search_array(*env, var);
+	var_index = search_array(s->env, var);
 	if (var_index >= 0)
-		ft_export_replace(*env, var, var_index);
+		ft_export_replace(s->env, var, var_index);
 	else
-		ft_export_append(env, var);
+		ft_export_append(&s->env, var);
 }
 
 /* Didn't think about many edge cases... */
@@ -164,23 +168,28 @@ char	*ft_getenv(char **env, char *key)
 	return (NULL);
 }
 
-// int	main(int ac, char **av, char **environ)
-// {
-// 	(void) ac;
-// 	(void) av;
-
-// 	environ = env_dup(environ);
-// 	// ft_pwd();
-// 	// ft_env(environ);
-// 	// ft_changeenv(environ);
-// 	// printf("------------------------\n");
-// 	// ft_env(environ);
-// 	// ft_mispoint(&environ);
-// 	// char **new_env;
-// 	// new_env = env_dup(environ);
-// 	ft_export(&environ, "HELLOOOOO=hi");
-// 	ft_print_strarr(environ);
-// 	printf("%s\n", getenv("HELLOOOOO"));
-// 	printf("%s\n", ft_getenv(environ, "HELLOOOOO"));
-// 	ft_free_str_arr(environ);
-// }
+int	main(int ac, char **av, char **env)
+{
+	(void) ac;
+	(void) av;
+	t_shell *s = malloc(sizeof(t_shell));
+	t_command *c = malloc(sizeof(t_command));
+	c->cmd = malloc(sizeof(*c->cmd) * 3);
+	for (int i = 0; i < 2; i++)
+		c->cmd[i] = malloc(sizeof(char) * 100);
+	ft_strlcpy(c->cmd[0], "export", 3);
+	if (ac > 1)
+		ft_strlcpy(c->cmd[1], av[1], ft_strlen(av[1]) + 1);
+	else
+		ft_strlcpy(c->cmd[1], "TRYME=bla", 10);
+	c->cmd[2] = NULL;
+	s->env = env_dup(env);
+	ft_export(s, c);
+	char *key =  ft_strtrunc(c->cmd[1], "=");
+	printf("%s: %s\n", c->cmd[1], ft_getenv(s->env, key));
+	free(key);
+	ft_free_str_arr(c->cmd);
+	ft_free_str_arr(s->env);
+	free(s);
+	free(c);
+}
