53bc8fe745d707b4596d651129c837bccfca92ac
tim <timotif@gmail.com>
2023-04-12 16:33:59 +0200

    vanilla implementation of builtin executor

--- minishell/executor.c
+++ minishell/executor.c
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:58:08 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/08 10:59:49 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/04/12 16:32:48 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,6 @@ t_command	*simple_parser(t_lexer *lex)
 		i++;
 	}
 	cmd->cmd[i] = NULL;
-	// printf("EXECUTOR\n*************\n");
-	// ft_print_strarr(cmd->cmd);
 	return (cmd);
 }
 
@@ -65,22 +63,61 @@ void	free_command(t_command *cmd)
 	free(cmd);
 }
 
+/* Checks if the cmd is a builtin and in that case
+returns its index number. Returns -1 if no match */
+int	find_builtin(t_shell *s, char *cmd)
+{
+	int	i;
+	int	len;
+
+	if (!cmd || !cmd[0])
+		return (-1);
+	i = 0;
+	len = ft_strlen(cmd);
+	while (s->builtins[i].name)
+	{
+		if (!ft_strncmp(s->builtins[i].name, cmd, len))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void	exec_builtin(t_shell *s, t_command *cmd, int builtin_idx)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		s->builtins[builtin_idx].func (s, cmd);
+		exit(0);
+	}
+}
+
 void	execute(t_shell *s, t_lexer *lex)
 {
 	t_command	*parsed_cmd;
 	pid_t		pid;
+	int			builtin_idx;
 
 	parsed_cmd = simple_parser(lex);
-	find_cmd(s, parsed_cmd->cmd[0]);
-	pid = fork();
-	if (pid == 0)
+	builtin_idx = find_builtin(s, parsed_cmd->cmd[0]);
+	if (builtin_idx >= 0)
+		exec_builtin(s, parsed_cmd, builtin_idx);
+	else
 	{
-		// printf("exec %s\n", parsed_cmd->cmd[0]);
-		execve(parsed_cmd->cmd[0], parsed_cmd->cmd, s->env);
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(parsed_cmd->cmd[0], 2);
-		ft_putstr_fd(": command not found\n", 2);
-		exit(1);
+		find_cmd(s, parsed_cmd->cmd[0]);
+		pid = fork();
+		if (pid == 0)
+		{
+			// printf("exec %s\n", parsed_cmd->cmd[0]);
+			execve(parsed_cmd->cmd[0], parsed_cmd->cmd, s->env);
+			ft_putstr_fd("minishell: ", 2);
+			ft_putstr_fd(parsed_cmd->cmd[0], 2);
+			ft_putstr_fd(": command not found\n", 2);
+			exit(1);
+		}
 	}
 	wait(NULL);
 	free_command(parsed_cmd);
