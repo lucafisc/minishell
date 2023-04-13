94c756bc9efa2a088832af1f9c4ea6edef8775cf
lucafisc <lucafischer@fastmail.com>
2023-04-13 11:49:33 +0200

    add strins to libft (refactor was segfaulting)

--- minishell/lex_expander.c
+++ minishell/lex_expander.c
@@ -6,43 +6,11 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 14:59:28 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/04/13 10:50:04 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/04/13 11:48:24 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/* It inserts s2 in s1 at the index idx substitung len chars of s1 */
-/* It doesn't free the arguments */
-char	*ft_strins(char *s1, char *s2, int len, int idx)
-{
-	int		len_s1;
-	char	*ret;
-	int		i;
-
-	if (!s1)
-		return (NULL);
-	len_s1 = ft_strlen(s1);
-	ret = malloc(sizeof(*ret) * (len_s1 + ft_strlen(s2) - len + 1));
-	if (!ret)
-		return (NULL);
-	i = -1;
-	while (++i < idx)
-		ret[i] = s1[i];
-	while (s2 && s2[i - idx])
-	{
-		ret[i] = s2[i - idx];
-		i++;
-	}
-	while (s1[idx + len])
-	{
-		ret[i] = s1[idx + len];
-		i++;
-		len++;
-	}
-	ret[i] = '\0';
-	return (ret);
-}
 
 char	*expand_var(char *cur, char *cmds, int i)
 {
--- minishell/libft/ft_strins.c
+++ minishell/libft/ft_strins.c
@@ -3,33 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strins.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lde-ross <lde-ross@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 11:58:13 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/07 12:09:33 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/04/13 11:47:36 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	copy_second_string(char *s2, char *ret, int idx, int *i)
-{
-	while (s2 && s2[*i - idx])
-	{
-		ret[*i] = s2[*i - idx];
-		i++;
-	}
-}
-
-static void	copy_remainder(char *s1, char *ret, int len, int idx, int *i)
-{
-	while (s1[idx + len])
-	{
-		ret[*i] = s1[idx + len];
-		(*i)++;
-		len++;
-	}
-}
 
 /* It inserts s2 in s1 at the index idx substitung len chars of s1 */
 /* It doesn't free the arguments */
@@ -48,9 +29,61 @@ char	*ft_strins(char *s1, char *s2, int len, int idx)
 	i = -1;
 	while (++i < idx)
 		ret[i] = s1[i];
-	if (s2)
-		copy_second_string(s2, ret, idx, &i);
-	copy_remainder(s1, ret, len, idx, &i);
+	while (s2 && s2[i - idx])
+	{
+		ret[i] = s2[i - idx];
+		i++;
+	}
+	while (s1[idx + len])
+	{
+		ret[i] = s1[idx + len];
+		i++;
+		len++;
+	}
 	ret[i] = '\0';
 	return (ret);
 }
+
+
+// static void	copy_second_string(char *s2, char *ret, int idx, int *i)
+// {
+// 	while (s2 && s2[*i - idx])
+// 	{
+// 		ret[*i] = s2[*i - idx];
+// 		i++;
+// 	}
+// }
+
+// static void	copy_remainder(char *s1, char *ret, int len, int idx, int *i)
+// {
+// 	while (s1[idx + len])
+// 	{
+// 		ret[*i] = s1[idx + len];
+// 		(*i)++;
+// 		len++;
+// 	}
+// }
+
+// /* It inserts s2 in s1 at the index idx substitung len chars of s1 */
+// /* It doesn't free the arguments */
+// char	*ft_strins(char *s1, char *s2, int len, int idx)
+// {
+// 	int		len_s1;
+// 	char	*ret;
+// 	int		i;
+
+// 	if (!s1)
+// 		return (NULL);
+// 	len_s1 = ft_strlen(s1);
+// 	ret = malloc(sizeof(*ret) * (len_s1 + ft_strlen(s2) - len + 1));
+// 	if (!ret)
+// 		return (NULL);
+// 	i = -1;
+// 	while (++i < idx)
+// 		ret[i] = s1[i];
+// 	if (s2)
+// 		copy_second_string(s2, ret, idx, &i);
+// 	copy_remainder(s1, ret, len, idx, &i);
+// 	ret[i] = '\0';
+// 	return (ret);
+// }
