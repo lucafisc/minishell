669ae22ff192b00312ea03f2916684ec0c9cedef
lucafisc <lucafischer@fastmail.com>
2023-04-24 09:57:09 +0200

    Remove comment

--- minishell/signal.c
+++ minishell/signal.c
@@ -6,7 +6,7 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 13:34:05 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/04/24 09:52:43 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/04/24 09:56:27 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	handler(int signal)
 		g_shell->status = 130;
 		printf("\n");
 		rl_on_new_line();
-		rl_replace_line("", 0); // clearing the current input line in the terminal.
+		rl_replace_line("", 0);
 		if (!g_shell->forked)
 			rl_redisplay();
 	}
