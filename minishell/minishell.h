1c3df4db6f1ec98d227f6305e763660d6fc524bb
Timoti Fregni <timotif@gmail.com>
2023-04-25 18:46:29 +0200

    added trim_quotes

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
+/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 16:02:25 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/24 13:50:32 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:45:57 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@
 # define SP_PARAM "@*#?-!"
 # define N_BUILTINS 7
 # define HEREDOC_NAME "_heredoc_temp"
+# define QUOTES "\'\""
 
 typedef enum s_bool
 {
@@ -117,6 +118,7 @@ void		par_fill_cmd(int *i, t_lexer *start, t_command **cmd_node);
 t_redir		is_redir(char *str);
 void		new_redir(int redir, t_lexer **lexer_node, t_command **cmd_node);
 void		setup_pipe(t_command *cmd, int n_cmds);
+char		*trim_quotes(char *data);
 
 /* ETC? */
 void		init_signal(void);
