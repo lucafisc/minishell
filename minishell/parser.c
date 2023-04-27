5b9bb58188be85e8276dd6fac07f7a5ab8a9683e
Luca Fischer <lucafischer@fastmail.com>
2023-04-27 16:21:58 +0200

    Split parser

--- minishell/parser.c
+++ minishell/parser.c
@@ -6,30 +6,29 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:31:54 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/27 16:02:50 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/04/27 16:20:00 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//t_shell	*g_shell;
+// t_shell	*g_shell;
 
-t_bool	is_pipe(char *str)
+t_bool is_pipe(char *str)
 {
 	if (str && *str && ft_strncmp(str, "|", 1) == 0)
 		return (true);
 	return (false);
 }
 
-void	setup_pipe(t_command *cmd, int n_cmds)
+void setup_pipe(t_command *cmd, int n_cmds)
 {
-	int	fd_p[2];
-	int	i;
+	int fd_p[2];
+	int i;
 
 	i = -1;
 	while (++i < n_cmds - 1)
 	{
-		// printf("PIPE #%d n_cmds: %d cmd: %s\n", i, n_cmds, cmd->cmd[0]);
 		if (pipe(fd_p) == -1)
 			throw_err("minishell", "pipe");
 		cmd->outfile = fd_p[1];
@@ -38,32 +37,42 @@ void	setup_pipe(t_command *cmd, int n_cmds)
 	}
 }
 
-t_command	*par_list_from_lex(t_lexer *lex, int n_cmds)
+int get_node_len(t_lexer **lex)
 {
-	t_command		*new;
-	t_lexer			*start;
-	int				len;
-	int				i;
+	int		len;
+	t_lexer	*current;
+
+	len = 0;
+	current = *lex;
+	while (current && !is_pipe(current->data))
+	{
+		if (is_redir(current->data))
+		{
+			current = current->next;
+			if (current)
+				current->info = LEX_FILE;
+		}
+		else
+			len++;
+		if (current)
+			current = current->next;
+	}
+	*lex = current;
+	return (len);
+}
+
+t_command *par_list_from_lex(t_lexer *lex, int n_cmds)
+{
+	t_command *new;
+	t_lexer *start;
+	int len;
+	int i;
 
 	i = 1;
 	while (lex && i <= n_cmds)
 	{
-		// printf("Processing %s\n", lex->data);
-		len = 0;
 		start = lex;
-		while (lex && !is_pipe(lex->data))
-		{
-			if (is_redir(lex->data))
-			{
-				lex = lex->next;
-				if (lex)
-					lex->info = LEX_FILE;
-			}
-			else
-				len++;
-			if (lex)
-				lex = lex->next;
-		}
+		len = get_node_len(&lex);
 		if (i == 1)
 			new = par_list_new_node(start, len);
 		else
@@ -78,9 +87,9 @@ t_command	*par_list_from_lex(t_lexer *lex, int n_cmds)
 	return (new);
 }
 
-int	par_count_cmds(t_lexer *lex)
+int par_count_cmds(t_lexer *lex)
 {
-	int	n_cmds;
+	int n_cmds;
 
 	n_cmds = 1;
 	while (lex)
@@ -92,9 +101,9 @@ int	par_count_cmds(t_lexer *lex)
 	return (n_cmds);
 }
 
-void	free_lex_list(t_lexer **lex)
+void free_lex_list(t_lexer **lex)
 {
-	t_lexer	*tmp;
+	t_lexer *tmp;
 
 	while (*lex)
 	{
@@ -105,19 +114,19 @@ void	free_lex_list(t_lexer **lex)
 	}
 }
 
-void	print_cmd_node(t_command **cmd)
+void print_cmd_node(t_command **cmd)
 {
-	t_command	*c;
+	t_command *c;
 
 	c = *cmd;
 	ft_print_strarr(c->cmd);
 }
 
-void	trim_cmd(t_command **cmd)
+void trim_cmd(t_command **cmd)
 {
-	int			i;
-	char		*temp;
-	t_command	*command;
+	int i;
+	char *temp;
+	t_command *command;
 
 	i = 0;
 	command = *cmd;
@@ -130,10 +139,10 @@ void	trim_cmd(t_command **cmd)
 	}
 }
 
-t_command	*parser(t_lexer *lex)
+t_command *parser(t_lexer *lex)
 {
-	t_command	*cmd;
-	int			n_cmds;
+	t_command *cmd;
+	int n_cmds;
 
 	n_cmds = par_count_cmds(lex);
 	cmd = par_list_from_lex(lex, n_cmds);
