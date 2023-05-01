011dc295043553a2af116230f824f24a20e48c97
Luca Fischer <lucafischer@fastmail.com>
2023-05-01 14:40:28 +0200

    multiple PIDS

--- minishell/executor.c
+++ minishell/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lde-ross <lde-ross@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:58:08 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/30 14:38:15 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/05/01 14:31:01 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,21 @@ void	child_routine(t_shell *s, t_command *command)
 	exit(1);
 }
 
-void	wait_for_child(int pid)
+void	wait_for_child(int *pids, int n_cmds)
 {
-	while (g_shell->status != 130 && waitpid(pid, NULL, WNOHANG) == 0)
+	int	i;
+
+	i = 0;
+	while (i > n_cmds - 1)
 	{
-		if (g_shell->status == 130)
-			kill(pid, SIGINT);
+		while (g_shell->status != 130 && waitpid(pids[i], NULL, WNOHANG) == 0)
+		{
+			if (g_shell->status == 130)
+				kill(pids[i], SIGINT);
+		}
+	i++;
 	}
+	
 }
 
 void	parent_routine(t_command **command)
@@ -55,10 +63,29 @@ void	parent_routine(t_command **command)
 	*command = tmp;
 }
 
+int		get_n_cmds(t_command *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd)
+	{
+		i++;
+		cmd = cmd->next;
+	}
+	return (i);
+}
+
 void	execute(t_shell *s, t_command *parsed_cmd)
 {
-	int			builtin_idx;
+	int	builtin_idx;
+	int	*pids;
+	int	n_cmds;
+	int	i;
 
+	n_cmds = get_n_cmds(parsed_cmd);
+	pids = malloc(sizeof(int) * n_cmds);
+	i = 0;
 	while (parsed_cmd)
 	{
 		builtin_idx = find_builtin(s, parsed_cmd->cmd[0]);
@@ -70,14 +97,15 @@ void	execute(t_shell *s, t_command *parsed_cmd)
 		{
 			trim_cmd(&parsed_cmd);
 			parsed_cmd->cmd[0] = find_cmd(s, parsed_cmd->cmd[0]);
-			s->pid = fork();
+			pids[i] = fork();
+			i++;
 			g_shell->forked = true;
 			if (s->pid == 0)
 				child_routine(s, parsed_cmd);
 		}
 		parent_routine(&parsed_cmd);
 	}
-	wait_for_child(s->pid);
+	wait_for_child(pids, n_cmds);
 	g_shell->forked = false;
 	add_status(g_shell->status);
 	g_shell->status = 0;
