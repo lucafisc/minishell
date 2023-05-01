b1030b152a21fda847b8750401677219e479b189
Timoti Fregni <timotif@gmail.com>
2023-05-01 22:08:31 +0200

    fixed export followed by valid param name without =

--- minishell/builtins/export.c
+++ minishell/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 15:19:22 by tfregni           #+#    #+#             */
-/*   Updated: 2023/04/30 14:45:02 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/05/01 22:05:03 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,19 @@ void	ft_export_append(char ***env, char *var)
 	*env = new_env;
 }
 
-/* Checks if input is a parameter */
-/* https://pubs.opengroup.org/onlinepubs/009695399/utilities
-/xcu_chap02.html#tag_02_05_02 */
-/* A param should have =, not start with a digit and not have
-any of the special chars macroed in SP_PARAM */
+t_bool	is_param_name(char *input)
+{
+	int	i;
+
+	i = -1;
+	while (input[++i])
+	{
+		if (ft_strchr(SP_PARAM, input[i]))
+			return (false);
+	}
+	return (true);
+}
+
 t_bool	is_param(char *input)
 {
 	int		i;
@@ -57,6 +65,38 @@ t_bool	is_param(char *input)
 	return (true);
 }
 
+/* Checks if input is a parameter */
+/* https://pubs.opengroup.org/onlinepubs/009695399/utilities
+/xcu_chap02.html#tag_02_05_02 */
+/* A param should not start with a digit and not have
+any of the special chars macroed in SP_PARAM. If it has a =
+it's valid to write or retrieve */
+// t_bool	is_param(char *input)
+// {
+// 	int		i;
+
+// 	i = -1;
+// 	if (!ft_strchr(input, '=') || ft_isdigit(*input) || *input == '=')
+// 		return (false);
+// 	while (++i <= (ft_strchr(input, '=') - input))
+// 	{
+// 		if (ft_strchr(SP_PARAM, input[i]))
+// 			return (false);
+// 	}
+// 	while (input[i])
+// 	{
+// 		if (input[i] && ft_strchr(QUOTES, input[i]))
+// 			i = ft_skip_char(input, input[i], i);
+// 		else if (input[i] && input[i] == '!' && input[i + 1])
+// 		{
+// 			ft_error("minishell", "event not found", input + i, 1);
+// 			return (false);
+// 		}
+// 		i++;
+// 	}
+// 	return (true);
+// }
+
 /* Export will set the variable also in the shell->params so that
 the expander can always get the latest occurrence */
 /* From the manual: When no arguments are given, the results are unspecified. */
@@ -69,17 +109,20 @@ void	ft_export(t_shell *s, t_command *c)
 		return ;
 	trim_cmd(&c);
 	var = c->cmd[1];
-	if (!is_param(var))
+	if (!is_param_name(var))
 	{
 		ft_error("minishell: export", "not a valid identifier", var, 1);
 		return ;
 	}
-	s->params = env_append(s->params, var);
-	var_index = search_array(s->env, var);
-	if (var_index >= 0)
-		ft_export_replace(s->env, var, var_index);
-	else
-		ft_export_append(&s->env, var);
+	if (is_param(var))
+	{
+		s->params = env_append(s->params, var);
+		var_index = search_array(s->env, var);
+		if (var_index >= 0)
+			ft_export_replace(s->env, var, var_index);
+		else
+			ft_export_append(&s->env, var);
+	}
 }
 
 // int	main(int ac, char **av, char **env)
